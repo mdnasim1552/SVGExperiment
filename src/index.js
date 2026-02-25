f8a241eea73b5d95bbbacd0c4bada0d4024f4933
MD. NASIM UDDIN <43935904+mdnasim1552@users.noreply.github.com>
2026-02-25 09:13:31 +0000

    Refactor branch configuration and attributes
    Refactor branch configuration and update organic stroke attributes.

--- src/index.js
+++ src/index.js
@@ -258,12 +258,13 @@ class Branch extends joint.dia.Link {
                     fill: '#aa0000',//colors.link,
                     stroke: '#aa0000',
                     strokeWidth: 3,
-                        strokeLinecap: 'round',
+                    strokeLinecap: 'round',
                     // Custom attributes
                     organicStroke: true,
                     organicStrokeSize: 20,
                     organicStrokeThinning:0,
-                    organicStrokeTaper:false
+                    organicStrokeTaper:false,
+                    organicStrokeStartCap:true,
                 },
             },
         };
@@ -372,7 +373,7 @@ class Branch extends joint.dia.Link {
                         cap:false
                     },
                     start:{
-                        cap:false
+                        cap: attrs['organic-stroke-start-cap'] ?? true,
                     }
                 });
                 // How to interpolate the points to get the outline?
@@ -387,6 +388,7 @@ class Branch extends joint.dia.Link {
         'organic-stroke-size': {},
         'organic-stroke-thinning': {}, // <-- new attribute
         'organic-stroke-taper': {},
+        'organic-stroke-start-cap': {}, // <-- new attribute
     };
 }
 // Stroke Style
@@ -2506,114 +2508,6 @@ joint.shapes.custom.Region = joint.dia.Element.define(
         ]
     }
 );
-const mporigin = { x: 1101, y: 449 };
-const mpLink = new Branch({
-    source: { x: 1123, y: 582 },
-    target: mporigin,
-    vertices: [{ x: 1157, y: 543 }, { x: 1169, y: 491 },{ x: 1155, y: 457 } 
-        ],
-    attrs: {
-        line: {
-            fill: '#C07BAE',//colors.link,
-            stroke: '#000000',
-            strokeWidth:1,
-            organicStrokeSize: 50,
-            organicStrokeThinning:.5,
-            organicStrokeTaper:0,
-        },
-    },
-});
-const myLink = new Branch({
-    source: {
-        id: mpLink.id,
-        anchor: { name: 'connectionRatio', args: { ratio: 0 } },
-    },
-    target: { x: 967, y: 1788 },
-    vertices: [{ x: 1078, y: 632 },{ x: 864, y: 1166 }, { x: 797, y: 1203 },{ x: 782, y: 1323 },{x:782,y:1431}
-        ,{x:765,y:1501},{x:815,y:1770},{x:861,y:1896},{x:894,y:1925},{x:935,y:1876}],
-    attrs: {
-        line: {
-            fill:"#EFE648",
-            stroke: '#000000',
-            strokeWidth:1,
-            organicStrokeSize: 30,
-            organicStrokeThinning:.3,
-            organicStrokeTaper:1,
-        },
-    },
-});
-const myLeftLink = new Branch({
-    source: {
-        id: mpLink.id,
-        anchor: { name: 'connectionRatio', args: { ratio: 0 } },
-    },
-    target: { x: 818, y: 816 },
-    vertices: [{ x: 1058, y: 620 },{ x: 977, y: 693 }, { x: 907, y: 735 }],
-    attrs: {
-        line: {
-            fill:"#EFE648",
-            stroke: '#000000',
-            strokeWidth:1,
-            organicStrokeSize: 25,
-            organicStrokeThinning:.3,
-            organicStrokeTaper:1,
-        },
-    },
-});
-const myLeftBLink = new Branch({
-    source: {
-        id: myLeftLink.id,
-        anchor: { name: 'connectionRatio', args: { ratio: 0.45 } },
-    },
-    target: { x: 915, y: 832 },
-    vertices: [{ x: 965, y: 736 },{ x: 953, y: 776 }],
-    attrs: {
-        line: {
-            fill:"#EFE648",
-            stroke: '#000000',
-            strokeWidth:1,
-            organicStrokeSize: 20,
-            organicStrokeThinning:.3,
-            organicStrokeTaper:1,
-        },
-    },
-});
-const myRightLink = new Branch({
-    source: {
-        id: mpLink.id,
-        anchor: { name: 'connectionRatio', args: { ratio: 0 } },
-    },
-    target: { x: 1241, y: 1290 },
-    vertices: [{ x: 1144, y: 628 },{ x: 1152, y: 839 }, { x: 1144, y: 954 },{ x: 1162, y: 1042 },{ x: 1235, y: 1199 }],
-    attrs: {
-        line: {
-            fill:"#EFE648",
-            stroke: '#000000',
-            strokeWidth:1,
-            organicStrokeSize: 25,
-            organicStrokeThinning:.3,
-            organicStrokeTaper:1,
-        },
-    },
-});
-const myRightBLink = new Branch({
-    source: {
-        id: myRightLink.id,
-        anchor: { name: 'connectionRatio', args: { ratio: 0.4 } },
-    },
-    target: { x: 1283, y: 1103 },
-    vertices: [{ x: 1169, y: 890 },{ x: 1219, y: 1001 }, { x: 1236, y: 1057 }],
-    attrs: {
-        line: {
-            fill:"#EFE648",
-            stroke: '#000000',
-            strokeWidth:1,
-            organicStrokeSize: 20,
-            organicStrokeThinning:.3,
-            organicStrokeTaper:1,
-        },
-    },
-});
 // const laorigin = { x: 862, y: 804 };
 
 // const laLink = new Branch({
@@ -2993,23 +2887,312 @@ const myRightBLink = new Branch({
 //         },
 //     ],
 // });
+
+const branchConfig = [
+  {
+    id: 'mpLink',
+    order: 99,
+    toFront: true,
+    source: { x: 1123, y: 582 },
+    target: { x: 1101, y: 449 },
+    vertices: [
+      { x: 1157, y: 543 }, { x: 1169, y: 491 },{ x: 1155, y: 457 }
+    ],
+    style: { fill: "#C07BAE",stroke: '#000000', strokeWidth:1, organicStrokeSize: 50, organicStrokeThinning: .5, organicStrokeTaper: 0, organicStrokeStartCap: false }
+  },
+  {
+    id: 'myLink',
+    order: 10,
+    parent: 'mpLink',
+    ratio: 0,
+    target: { x: 967, y: 1788 },
+    vertices: [
+      { x: 1078, y: 632 },{x:1014,y:761},{x:931,y:927},{ x: 864, y: 1066 }, { x: 797, y: 1203 },{ x: 782, y: 1323 },{x:782,y:1431}
+        ,{x:765,y:1501},{x:815,y:1770},{x:861,y:1896},{x:894,y:1925},{x:935,y:1876}
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:1, organicStrokeSize: 35, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myLeftLink',
+    order: 3,
+    parent: 'mpLink',
+    ratio: 0,
+    target: { x: 818, y: 816 },
+    vertices: [
+      { x: 1058, y: 620 },{ x: 977, y: 693 }, { x: 907, y: 735 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 30, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myLeftBLink',
+    order: 2,
+    parent: 'myLeftLink',
+    ratio: 0.45,
+    target: { x: 915, y: 832 },
+    vertices: [
+        { x: 965, y: 736 },{ x: 953, y: 776 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myLeftCLink',
+    order: 3,
+    parent: 'myLink',
+    ratio: 0.18,
+    target: { x: 734, y: 959 },
+    vertices: [
+        { x: 939, y: 867 },{ x: 880, y: 882 }, { x: 803, y: 959 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myLeftDLink',
+    order: 3,
+    parent: 'myLink',
+    ratio: 0.3,
+    target: { x: 693, y: 1165 },
+    vertices: [
+        { x: 836, y: 1042 },{ x: 767, y: 1114 }, { x: 716, y: 1128 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myRightLink',
+    order: 3,
+    parent: 'mpLink',
+    ratio: 0,
+    target: { x: 1241, y: 1290 },
+    vertices: [
+        { x: 1144, y: 628 },{ x: 1152, y: 839 }, { x: 1144, y: 954 },{ x: 1162, y: 1042 },{ x: 1235, y: 1199 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 30, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myRightBLink',
+    order: 2,
+    parent: 'myRightLink',
+    ratio: 0.4,
+    target: { x: 1283, y: 1103 },
+    vertices: [
+        { x: 1169, y: 890 },{ x: 1219, y: 1001 }, { x: 1236, y: 1057 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myRightCLink',
+    order: 3,
+    parent: 'myLink',
+    ratio: 0.17,
+    target: { x: 1102, y: 1229 },
+    vertices: [
+        { x: 1002, y: 924 },{ x: 999, y: 1001 },{ x: 1036, y: 1058 },{ x: 1045, y: 1158 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myRightDLink',
+    order: 2,
+    parent: 'myRightCLink',
+    ratio: 0.17,
+    target: { x: 1079, y: 1052 },
+    vertices: [
+        { x: 1036, y: 955 },{ x: 1079, y: 1015 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myRightELink',
+    order: 3,
+    parent: 'myLink',
+    ratio: 0.32,
+    target: { x: 934, y: 1277 },
+    vertices: [
+        { x: 897, y: 1101 },{ x: 900, y: 1209 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'myRightFLink',
+    order: 2,
+    parent: 'myRightELink',
+    ratio: 0.37,
+    target: { x: 959, y: 1211 },
+    vertices: [
+        { x: 941, y: 1163 }
+    ],
+    style: { fill:"#EFE648", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0Link',
+    order: 10,
+    source: { x: 1097, y: 506 },
+    target: { x: 1491, y: 1294 },
+    vertices: [
+      { x: 1124, y: 532 },{ x: 1212, y: 517 },{x:1331,y:510},{x:1443,y:576},{ x: 1541, y: 674 }, { x: 1611, y: 786 },{ x: 1654, y: 895 },{x:1657,y:1007},
+        {x:1632,y:1109},{x:1569,y:1214}
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:1, organicStrokeSize: 50, organicStrokeThinning:.3, organicStrokeTaper:0,organicStrokeStartCap: false }
+  },
+  {
+    id: 'mB0LinkChild01',
+    order: 3,
+    parent: 'mB0Link',
+    ratio: 0.15,
+    target: { x: 1297, y: 440 },
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:0.8}
+  },
+  {
+    id: 'mB0LinkChild1',
+    order: 3,
+    parent: 'mB0Link',
+    ratio: 0.1,
+    target: { x: 1305, y: 1016 },
+    vertices: [
+        { x: 1273, y: 573 },{ x: 1289, y: 637 },{ x: 1273, y: 730 },{ x: 1261, y: 782 },{ x: 1273, y: 871 },{ x: 1305, y: 931 }
+    ],
+    style: { fill:"#6CBE47", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild2',
+    order: 2,
+    parent: 'mB0LinkChild1',
+    ratio: 0.4,
+    target: { x: 1362, y: 847 },
+    vertices: [
+        { x: 1305, y: 806 }
+    ],
+    style: { fill:"#6CBE47", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 20, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild3',
+    order: 3,
+    parent: 'mB0Link',
+    ratio: 0.2,
+    target: { x: 1382, y: 1012 },
+    vertices: [
+        { x: 1378, y: 597 },{ x: 1426, y: 677 },{ x: 1439, y: 758 },{ x: 1430, y: 851 },{ x: 1430, y: 935 }
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild4',
+    order: 2,
+    parent: 'mB0LinkChild3',
+    ratio: 0.5,
+    target: { x: 1515, y: 891 },
+    vertices: [
+        { x: 1463, y: 790 },{ x: 1479, y: 851 }
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild5',
+    order: 3,
+    parent: 'mB0Link',
+    ratio: 0.58,
+    target: { x: 1354, y: 1113 },
+    vertices: [
+        { x: 1620, y: 859 },{ x: 1576, y: 935 },{ x: 1527, y: 996 },{ x: 1475, y: 1012 },{ x: 1430, y: 1080 },{ x: 1394, y: 1113 }
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild6',
+    order: 2,
+    parent: 'mB0LinkChild5',
+    ratio: 0.5,
+    target: { x: 1471, y: 1101 },
+    vertices: [
+        { x: 1515, y: 1024 },{ x: 1503, y: 1072 }
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild7',
+    order: 3,
+    parent: 'mB0Link',
+    ratio: 0.74,
+    target: { x: 1334, y: 1246 },
+    vertices: [
+        { x: 1620, y: 1048 },{ x: 1531, y: 1137 },{ x: 1402, y: 1181 }
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+  {
+    id: 'mB0LinkChild8',
+    order: 2,
+    parent: 'mB0LinkChild7',
+    ratio: 0.4,
+    target: { x: 1503, y: 1205 },
+    vertices: [
+        { x: 1531, y: 1161 }
+    ],
+    style: { fill:"#3679BD", stroke: '#000000', strokeWidth:0.5, organicStrokeSize: 25, organicStrokeThinning:.3, organicStrokeTaper:1 }
+  },
+];
+//const sortedConfig = [...branchConfig].sort((a, b) => a.order - b.order);
+const createdLinks = {};
+const cells = [];
+
+branchConfig.forEach(cfg => {
+
+  // Build source
+  let source;
+
+  if (cfg.parent) {
+    source = {
+      id: createdLinks[cfg.parent].id,
+      anchor: {
+        name: 'connectionRatio',
+        args: { ratio: cfg.ratio ?? 0 }
+      }
+    };
+  } else {
+    source = cfg.source;
+  }
+const lineAttrs = {
+  ...(cfg.style?.fill !== undefined && { fill: cfg.style.fill }),
+  ...(cfg.style?.stroke !== undefined && { stroke: cfg.style.stroke }),
+  ...(cfg.style?.strokeWidth !== undefined && { strokeWidth: cfg.style.strokeWidth }),
+  ...(cfg.style?.organicStrokeSize !== undefined && { organicStrokeSize: cfg.style.organicStrokeSize }),
+  ...(cfg.style?.organicStrokeThinning !== undefined && { organicStrokeThinning: cfg.style.organicStrokeThinning }),
+  ...(cfg.style?.organicStrokeTaper !== undefined && { organicStrokeTaper: cfg.style.organicStrokeTaper }),
+  ...(cfg.style?.organicStrokeStartCap !== undefined && { organicStrokeStartCap: cfg.style.organicStrokeStartCap })
+};
+  const link = new Branch({
+    source,
+    target: cfg.target,
+    vertices: cfg.vertices || [],
+    ...(Object.keys(lineAttrs).length && {
+        attrs: { line: lineAttrs }
+    })
+  });
+if(cfg.order) {
+    //link.toFront();
+    link.set('z', cfg.order);
+}
+  createdLinks[cfg.id] = link;
+  cells.push(link);
+});
 const region = new joint.shapes.custom.Region({
     position: { x: 0, y: 0 },
 });
-graph.addCells([
-    region,
-    mpLink,
-    myLeftLink,
-    myRightLink,
-    myLink,
-    myLeftBLink,
-    myRightBLink
-    // rootLink,
-    // outerLink,
-    // outerLink2,
-    // outerLink3,
-    // outerLink4
-]);
+graph.addCells([region]);
+graph.addCells(cells);
+// graph.addCells([
+//     region,
+//     mpLink,
+//     myLeftLink,
+//     myRightLink,
+//     myLink,
+//     myLeftBLink,
+//     myRightBLink
+//     // rootLink,
+//     // outerLink,
+//     // outerLink2,
+//     // outerLink3,
+//     // outerLink4
+// ]);
 // Fit the content of the paper to the viewport.
 // ---------------------------------------------
 paper.transformToFitContent({

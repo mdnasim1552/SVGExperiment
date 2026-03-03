d486cdecb04d7ee149252699eef31a1873904cce
MD. NASIM UDDIN <43935904+mdnasim1552@users.noreply.github.com>
2026-03-03 09:29:49 +0000

    Update webpack config for production and add CSS support

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,32 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader, // 👈 extracts to file
+          'css-loader'
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
 };
